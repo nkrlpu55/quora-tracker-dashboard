d652b2cbc53b7e91a038557b33762fd2b8139497
Nachiketa <nkr.lpu55@gmail.com>
2026-01-28 16:50:03 +0530

    Test commit

--- vite-project/src/pages/MyTasks.jsx
+++ vite-project/src/pages/MyTasks.jsx
@@ -180,6 +180,7 @@ export default function MyTasks() {
           )}
 
           {/* SHOW INPUT ONLY IF PENDING */}
+          {/* Push test check */}
           {task.status === "pending" && (
             <>
               <input
